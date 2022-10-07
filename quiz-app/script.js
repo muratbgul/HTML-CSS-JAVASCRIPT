1a166a751884d955bf66d3e94811c9c60bb16476
murat <beyazgul.murat2@gmail.com>
2022-10-07 22:55:39 +0300

    Constructor

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -1,30 +1,59 @@
-let soru = {
-    soruMetni:"Hangisi javascript paket yönetim uygulamasıdır?",
-    cevapSecenekleri:{
-        a:"Node.js",
-        b:"Typescript",
-        c:"Npm"
-    },
-    dogruCevap:"c",
-    cevabiKontrolEt: function(cevap){
+
+
+// let soru = {
+//     soruMetni:"Hangisi javascript paket yönetim uygulamasıdır?",
+//     cevapSecenekleri:{
+//         a:"Node.js",
+//         b:"Typescript",
+//         c:"Npm"
+//     },
+//     dogruCevap:"c",
+//     cevabiKontrolEt: function(cevap){
+//         return cevap === this.dogruCevap
+//     }
+// }
+
+// let soru2 = {
+//     soruMetni:"Hangisi .net paket yönetim uygulamasıdır?",
+//     cevapSecenekleri:{
+//         a:"Node.js",
+//         b:"Nugget",
+//         c:"Npm"
+//     },
+//     dogruCevap:"b", 
+//     cevabiKontrolEt: function(cevap){
+//         return cevap === this.dogruCevap
+//     }
+// }
+
+function Soru(soruMetni,cevapSecenekleri,dogruCevap) {
+    this.soruMetni = soruMetni;
+    this.cevapSecenekleri = cevapSecenekleri;
+    this.dogruCevap = dogruCevap;
+    this.cevabiKontrolEt = function(cevap){
         return cevap === this.dogruCevap
     }
 }
 
-let soru2 = {
-    soruMetni:"Hangisi .net paket yönetim uygulamasıdır?",
-    cevapSecenekleri:{
-        a:"Node.js",
-        b:"Nugget",
-        c:"Npm"
-    },
-    dogruCevap:"b", 
-    cevabiKontrolEt: function(cevap){
-        return cevap === this.dogruCevap
-    }
-}
+let soru1 = new Soru("Hangisi javascript paket yönetim uygulamasıdır?",{a:"Node.js",b:"Typescript",c:"Npm"},"c");
+let soru2 = new Soru("Hangisi .net paket yönetim uygulamasıdır?",{a:"Node.js",b:"Nugget",c:"Npm"},"b");
 
-console.log(soru.soruMetni);
-console.log(soru.cevabiKontrolEt("c"));
-console.log(soru2.soruMetni);
-console.log(soru2.cevabiKontrolEt("b"));
+let sorular = [
+    new Soru("Hangisi 1 paket yönetim uygulamasıdır?",{a:"Node.js",b:"Typescript",c:"Npm"},"c"),
+    new Soru("Hangisi 2 paket yönetim uygulamasıdır?",{a:"Node.js",b:"Typescript",c:"Npm"},"c"),
+    new Soru("Hangisi 3 paket yönetim uygulamasıdır?",{a:"Node.js",b:"Typescript",c:"Npm"},"c"),
+    new Soru("Hangisi 4 paket yönetim uygulamasıdır?",{a:"Node.js",b:"Typescript",c:"Npm"},"c")
+]
+
+// console.log(sorular[0].soruMetni); 
+
+// for(let s of sorular){
+//     console.log(s.soruMetni);
+// }
+
+console.log(soru1.soruMetni);
+console.log(soru1.cevapSecenekleri);
+console.log(soru1.cevabiKontrolEt("c"));
+
+console.log(sorular[0].cevabiKontrolEt("c"));
+
